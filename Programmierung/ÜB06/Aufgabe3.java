bac584bbbb62227f55a92deb614b39406875bd26
Zeltik <27410308+Zeltik@users.noreply.github.com>
2021-12-01 11:30:04 +0100

    Update Aufgabe3.java
    Volle Ausgabe immer noch falschrum

--- Programmierung/ÜB06/Aufgabe3.java
+++ Programmierung/ÜB06/Aufgabe3.java
@@ -8,14 +8,21 @@ public class Aufgabe3 {
     }
 
     public static void stepSum(int[] input) {
-        int[] result = new int[input.length - 1];
+        int[] result = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            result[i] = input[i];
+        }            for (int i = 0; i < result.length; i++) {
+                System.out.print(result[i] + " ");
+            }
+        result = new int[input.length - 1];
         if (input.length > 1) {
+
+        	
+
             for (int i = 1; i < input.length; i++) {
                 result[i - 1] = input[i] + input[i - 1];
             }
-            for (int i = 0; i < result.length; i++) {
-                System.out.print(result[i] + " ");
-            }
+
             System.out.println();
             stepSum(result);
         }
